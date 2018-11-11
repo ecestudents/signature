0a08879e1b47e3ee7f95f529d16fc72cfa917762
Tim <treasurer@ecestudents.nl>
2018-11-11 21:43:25 +0000

    siganture page

--- src/pages/signature.js
+++ src/pages/signature.js
@@ -8,9 +8,7 @@ export default class IndexPage extends React.Component {
         <section>
           <img src="https://res.cloudinary.com/ece/image/upload/f_auto,q_auto,h_80/parts/ECEStudents_logo_2018.png" />
           <br />
-          <i>
-            Hi this the new website of ECE Students, for questions ask Jenny.
-          </i>
+          <i>This going to be the signature generator page</i>
         </section>
       </Layout>
     );
