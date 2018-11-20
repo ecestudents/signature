9f6cdff5505a2d63e83b0008231a85351379b0c9
Tim <treasurer@ecestudents.nl>
2018-11-20 14:56:57 +0000

    color generator

--- src/components/styles/_gradient.js
+++ src/components/styles/_gradient.js
@@ -1,12 +1,31 @@
-import { lighten, darken } from "polished";
+import { parseToHsl, hsl } from "polished";
 
-const Gradient = (color = "#e55625") => `
+const Gradient = (color = "#e55625") => {
+  let start = parseToHsl(color);
+  let end = parseToHsl(color);
+
+  start.hue += 12;
+  start.saturation *= 1.25;
+  start.lightness += 0.02;
+  //console.log(start);
+
+  /*
+  normal H:15 S:79% L:52% A:1
+  start H:27 S:100% L:54% A:1
+  end H:16 S:78% L:52% A:1
+   
+  */
+
+  console.log(start);
+
+  return `
   background-color: ${color};
   background: linear-gradient(
     90deg,
-    ${lighten(0.04, color)} 0,
-    ${darken(0.06, color)}
+    ${hsl(start)} 0,
+    ${hsl(end)}
   );
 `;
+};
 
 export default Gradient;
