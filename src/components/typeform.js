076a3237c1ff04fe264729b6848d1e66f4edabd2
Tim <treasurer@ecestudents.nl>
2018-11-25 23:14:48 +0000

    front only still typeform

--- src/components/typeform.js
+++ src/components/typeform.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { styled, Gradient, ms } from "styles";
 import { ReactTypeformEmbed } from "react-typeform-embed";
 
+const Popup = styled.div`
+  display: none;
+`;
+
+const Button = styled.button`
+  ${props => (props.gradient ? Gradient(props.gradient) : Gradient())}
+  cursor: pointer;
+  color: white;
+  padding: ${ms(-1)} ${ms(2)};
+  font-size: ${ms(1.5)};
+  text-decoration: none;
+  border-radius: 10px;
+  text-transform: uppercase;
+  border: 0;
+`;
 class Typeform extends React.Component {
   constructor(props) {
     super(props);
@@ -15,26 +31,20 @@ class Typeform extends React.Component {
   render() {
     return (
       <div>
-        <div>
+        <Popup>
           <ReactTypeformEmbed
             popup={true}
             autoOpen={false}
             url={"https://ecestudents2018.typeform.com/to/" + this.props.form}
             hideHeaders={true}
             hideFooter={true}
-            style={{ top: 100 }}
+            style={{ top: 10 }}
             ref={tf => (this.typeformEmbed = tf)}
           />
-        </div>
-        <div>
-          <button
-            className="btn"
-            onClick={this.openForm}
-            style={{ cursor: "pointer" }}
-          >
-            {this.props.text}
-          </button>
-        </div>
+        </Popup>
+        <Button onClick={this.openForm} gradient={this.props.gradient}>
+          {this.props.text}
+        </Button>
       </div>
     );
   }
