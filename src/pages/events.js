33c02f9413821664eb16a9fbac6521e9bc620b47
Tim <treasurer@ecestudents.nl>
2019-01-02 20:14:10 +0000

    begin event dingen

---
+++ src/pages/events.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../components/layout";
+import { Wrapper, styled } from "styles";
+
+const Content = styled(Wrapper)``;
+
+const EventsPage = () => (
+  <Layout>
+    <Content>
+      <h1>EVENTS PAGE</h1>
+      <p>Sorry we cannot find the page you are looking for.</p>
+    </Content>
+  </Layout>
+);
+
+export default EventsPage;
---
+++ src/pages/team.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../components/layout";
+import { Wrapper } from "styles";
+
+const Team = () => (
+  <Layout>
+    <Wrapper>
+      <div>
+        <h1>The Team</h1>
+        <p />
+      </div>
+    </Wrapper>
+  </Layout>
+);
+
+export default Team;
